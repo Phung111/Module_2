ce49a3375034954fe1822c5e7554c304c803e279
unknown <men01225423493@gmail.com>
2022-12-12 21:19:25 +0700

    9.19.12.12

--- 6.Inheritance/Shape/src/Test/Circle.java
+++ 6.Inheritance/Shape/src/Test/Circle.java
@@ -1,6 +1,6 @@
 package Test;
 
-public class Circle extends Geometric{
+public class Circle{
     private int radius;
     public Circle(int radius) {
         this.radius = radius;
---
+++ 6.Inheritance/Shape/src/Triangle/TriangleTest.java
@@ -0,0 +1,9 @@
+package Triangle;
+import Shape.Shape;
+
+public class TriangleTest {
+    public static void main(String[] args) {
+        Shape triangle = new Triangle(2,2,3);
+        System.out.println(triangle);
+    }
+}
